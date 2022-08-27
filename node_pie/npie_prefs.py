1c7f5611365d44f3942e8229145659bd970001a0
strike-digital <a@sunadal.co.uk>
2022-08-27 01:04:51 +0100

    Added option to turn off showing node groups in the pie menu

--- node_pie/npie_prefs.py
+++ node_pie/npie_prefs.py
@@ -13,9 +13,29 @@ class NodePiePrefs(bpy.types.AddonPreferences):
     layout: UILayout
     node_pie_enabled: BoolProperty(name="Enable node pie", default=True)
 
-    npie_variable_sizes: BoolProperty(name="Use variable size", default=True)
-    npie_normal_size: FloatProperty(name="Normal size", default=1)
-    npie_max_size: FloatProperty(name="Max size", default=2.5)
+    npie_variable_sizes: BoolProperty(
+        name="Use variable size",
+        default=True,
+        description="Whether to increase the size of node buttons that are used most often",
+    )
+
+    npie_normal_size: FloatProperty(
+        name="Normal size",
+        default=1,
+        description="The default size of the nodes buttons",
+    )
+
+    npie_max_size: FloatProperty(
+        name="Max size",
+        default=2.5,
+        description="The size of the most popular nodes",
+    )
+
+    npie_show_node_groups: BoolProperty(
+        name="Show node groups",
+        default=True,
+        description="Whether to show node groups in the pie menu or not",
+    )
 
     npie_color_size: FloatProperty(
         name="Color bar size",
@@ -38,17 +58,19 @@ class NodePiePrefs(bpy.types.AddonPreferences):
         prefs = get_prefs(context)
         layout = layout.grid_flow(row_major=True, even_columns=True)
 
-        col = draw_section(layout, "Node Popularity")
         fac = .515
+        col = draw_section(layout, "Node Popularity")
         draw_inline_prop(col, prefs, "npie_variable_sizes", factor=fac)
-        draw_inline_prop(col, prefs, "npie_color_size", factor=fac)
         draw_inline_prop(col, prefs, "npie_normal_size", factor=fac)
         draw_inline_prop(col, prefs, "npie_max_size", factor=fac)
-
         row = col.row(align=True)
         row.scale_y = 1.5
         row.prop(prefs, "npie_freeze_popularity", text="Freeze popularity", icon="FREEZE", toggle=True)
         row.operator("node_pie.reset_popularity", icon="FILE_REFRESH")
+
+        col = draw_section(layout, "General")
+        draw_inline_prop(col, prefs, "npie_show_node_groups", factor=fac)
+        draw_inline_prop(col, prefs, "npie_color_size", factor=fac)
 
         col = draw_section(layout, "Keymap")
         global addon_keymaps
