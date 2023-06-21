c642e8adbffd94c329c6c9874fa92282a5e504ad
Strike Digital <59890307+strike-digital@users.noreply.github.com>
2023-06-21 01:38:36 +0100

    Changed config file versioning system

--- node_pie/generate_node_layout.py
+++ node_pie/generate_node_layout.py
@@ -47,41 +47,68 @@ data = {}
 
 def main():
 
-    for area in bpy.context.window.screen.areas:
-        if area.type == "NODE_EDITOR":
-            with bpy.context.temp_override(area=area):
-                cats = list(nodeitems_utils.node_categories_iter(bpy.context))
-                for cat in cats:
-                    # if cat.name not in colours.keys():
-                    #     continue
-                    items = []
-                    for item in cat.items(bpy.context):
-                        if not isinstance(item, nodeitems_utils.NodeItem):
-                            continue
-                        settings = item.settings
-                        data_item = {"label": item.label, "identifier": item.nodetype}
-                        if settings:
-                            data_item["settings"] = settings
-                        if item.nodetype in color_overrides:
-                            data_item["color"] = color_overrides[item.nodetype]
-                        items.append(data_item)
-                    data[cat.identifier] = {"label": cat.name, "color": "input", "nodes": items}
+    # for area in bpy.context.window.screen.areas:
+    #     if area.type == "NODE_EDITOR":
+    #         with bpy.context.temp_override(area=area):
+    #             cats = list(nodeitems_utils.node_categories_iter(bpy.context))
+    #             for cat in cats:
+    #                 # if cat.name not in colours.keys():
+    #                 #     continue
+    #                 items = []
+    #                 for item in cat.items(bpy.context):
+    #                     if not isinstance(item, nodeitems_utils.NodeItem):
+    #                         continue
+    #                     settings = item.settings
+    #                     data_item = {"label": item.label, "identifier": item.nodetype}
+    #                     if settings:
+    #                         data_item["settings"] = settings
+    #                     if item.nodetype in color_overrides:
+    #                         data_item["color"] = color_overrides[item.nodetype]
+    #                     items.append(data_item)
+    #                 data[cat.identifier] = {"label": cat.name, "color": "input", "nodes": items}
 
-                selected_nodes = bpy.context.selected_nodes
-                items = []
-                for node in selected_nodes:
-                    data_item = {"label": node.bl_label, "identifier": node.bl_idname}
-                    items.append(data_item)
+    #             selected_nodes = bpy.context.selected_nodes
+    #             items = []
+    #             for node in selected_nodes:
+    #                 data_item = {"label": node.bl_label, "identifier": node.bl_idname}
+    #                 items.append(data_item)
 
-    # Uncomment to write out the initial file from an old version that still has a working NodeItems api.
-    # Shouldn't need to be used now.
-    if data:
-        fpath = NODE_DEF_DIR / "CompositorNodeTree2.jsonc"
-        with open(fpath, "w") as f:
-            json.dump(data, f, indent=2)
+    # # Uncomment to write out the initial file from an old version that still has a working NodeItems api.
+    # # Shouldn't need to be used now.
+    # if data:
+    #     fpath = NODE_DEF_DIR / "CompositorNodeTree2.jsonc"
+    #     with open(fpath, "w") as f:
+    #         json.dump(data, f, indent=2)
 
-        print("Dumped!")
+    #     print("Dumped!")
     # print(data)
+
+    orig_file = NODE_DEF_DIR / "GeometryNodeTree.jsonc"
+    new_file = NODE_DEF_DIR / "GeometryNodeTree_40 copy.jsonc"
+
+    with open(orig_file, "r") as f:
+        orig_data = json.load(f)
+    with open(new_file, "r") as f:
+        new_data = json.load(f)
+
+    orig_categories = orig_data["categories"]
+    new_categories = new_data["categories"]
+    data = {}
+
+    for orig_name, orig_cat in orig_categories.items():
+        new_cat = new_categories[orig_name]
+        nodes = []
+        for node in new_cat["nodes"]:
+            if node.get("identifier") not in {n.get("identifier") for n in orig_cat["nodes"]}:
+                if node["label"] != "sep":
+                    nodes.append(node)
+        if nodes:
+            data[orig_name] = {"nodes": nodes}
+
+    # print(dict(new_categories.items() - orig_categories.items()))
+    data.update({k: v for k, v in new_categories.items() if k not in orig_categories})
+
+    print(json.dumps(data, indent=2) + ",")
 
 
 # Uncomment to call main function
--- node_pie/npie_custom_pies.py
+++ node_pie/npie_custom_pies.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+import collections
 from dataclasses import dataclass, field
 import json
 
@@ -36,7 +38,6 @@ class Separator():
 
 def load_custom_nodes_info(tree_identifier: str) -> tuple[dict[str, NodeCategory], dict]:
     bl_version = bpy.app.version
-    all_nodes = {}
     categories = {}
     layout = {}
 
@@ -46,25 +47,76 @@ def load_custom_nodes_info(tree_identifier: str) -> tuple[dict[str, NodeCategory
         if file.is_file() and file.suffix == ".jsonc" and file.name.startswith(f"{tree_identifier}"):
             files[file.stem.split("_")[-1]] = file
 
+    for file in files.copy():
+        if file != tree_identifier:
+            try:
+                int(file)
+            except ValueError:
+                del files[file]
+                continue
+            if int(file) > int(f"{bl_version[0]}{bl_version[1]}"):
+                del files[file]
+
     if not files:
         return {}, {}
 
-    # Load the latest version
-    def_path = ""
-    version = int(f"{bl_version[0]}{bl_version[1]}")
-    while True:
-        try:
-            def_path = files[str(version)]
-            break
-        except KeyError:
-            version -= 1
-            if version < 0:
-                break
-            continue
-    def_path = def_path or list(files.values())[-1]
+    def sort(f):
+        if f[0] != tree_identifier:
+            return int(f[0])
+        return 0
 
-    with open(def_path, "r") as f:
+    files = OrderedDict(sorted(list(files.items()), key=sort))
+
+    with open(list(files.values())[0], "r") as f:
         data = json.load(f, cls=JSONWithCommentsDecoder)
+
+    for file in list(files.values())[1:]:
+        with open(file, "r") as f:
+            new_data = json.load(f, cls=JSONWithCommentsDecoder)
+
+        # Merge layout
+        for orig_area_name, orig_columns in data["layout"].items():
+            new_columns = new_data["additions"]["layout"].get(orig_area_name)
+            if not new_columns:
+                continue
+            for i, new_column in enumerate(new_columns):
+                for new_row in new_column:
+                    orig_columns[i].append(new_row)
+
+        # Merge in the new nodes
+        for orig_cat_name, orig_cat in data["categories"].items():
+            new_cat = new_data["additions"]["categories"].get(orig_cat_name)
+            if new_cat:
+                # Insert the node after the specified one.
+                names = [n.get("identifier") for n in orig_cat["nodes"]]
+                idx = -1
+                for new_node in new_cat["nodes"]:
+                    if name := new_node.get("after_node"):
+                        idx = names.index(name)
+                    if idx == -1:
+                        orig_cat["nodes"].append(new_node)
+                    else:
+                        orig_cat["nodes"].insert(idx + 1, new_node)
+
+        # Add new categories
+        new_cats = new_data["additions"]["categories"].keys() - data["categories"].keys()
+        for new_cat in new_cats:
+            data["categories"][new_cat] = new_data["additions"]["categories"][new_cat]
+        # data["categories"].update(new_data["additions"]["categories"])
+
+    # Load the latest version
+    # def_path = ""
+    # version = int(f"{bl_version[0]}{bl_version[1]}")
+    # while True:
+    #     try:
+    #         def_path = files[str(version)]
+    #         break
+    #     except KeyError:
+    #         version -= 1
+    #         if version < 0:
+    #             break
+    #         continue
+    # def_path = def_path or list(files.values())[-1]
 
     layout = data["layout"]
 
