12fae45dfdc1ab007450123a127209bba00759aa
Strike Digital <59890307+strike-digital@users.noreply.github.com>
2025-10-05 12:38:04 +0100

    version bump

--- __init__.py
+++ __init__.py
@@ -13,11 +13,11 @@
 import bpy
 
 bl_info = {
-    "name": "Node Pie 1.2.48",
+    "name": "Node Pie 1.2.49",
     "author": "Andrew Stevenson",
     "description": "Add nodes quickly with a pie menu",
     "blender": (4, 2, 0),
-    "version": (1, 2, 48),
+    "version": (1, 2, 49),
     "location": "Node editor > Shortcut",
     "doc_url": "https://github.com/strike-digital/node_pie/wiki",
     "tracker_url": "https://github.com/strike-digital/node_pie/issues",
